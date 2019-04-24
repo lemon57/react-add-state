9aea39f52abd1289ddf5e5c29c525bf8073a9d33
Roman Lemon <gzeek57@gmail.com>
2019-04-24 12:25:47 +0200

    add state: when click on div add ot remove class clicked

---
+++ lib/01_article_with_state.jsx
@@ -0,0 +1,33 @@
+// TODO: update dependencies if needed
+import React, {Component} from 'react';
+
+// TODO: transform the following functional component into a class based component
+// TODO: set the initial state { clicked: false }
+// TODO: listen to the click event on `.article`
+// TODO: code the `handleClick` function (which adds a 'clicked' class to the `.article`)
+class Article extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  }
+
+  render () {
+    return (
+      <div className={this.state.clicked ? 'article clicked' : 'article'} onClick={this.handleClick}>
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
+      </div>
+    );
+  }
+}
+
+export default Article;
